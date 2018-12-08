ab4cec766f907e16fb379bbe4bcc6e35b3d3b1ec
mango dyl <dylanbonds@hotmail.com>
2018-12-08 10:16:45 -0600

    characters move to places on first click

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,6 +12,8 @@ $(".card").on("click", function() {
     $(this).detach().appendTo("#your-fighter-row");
     console.log("ok");
 
+    $(".card").not(this).addClass("enemy");
 
+    $(".enemy").detach().appendTo("#available-fighter-row");
 
 });
